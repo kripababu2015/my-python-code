ae49ca46e662bc15f6ff0c238cb6bf9647c37361
kripababu2015 <Administrator@MCA-33.mca.com>
2019-02-27 12:47:35 +0530

    add flakes update commit

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     return "hello.."
+@app.route("/home")
+def home():
+    return "<h1> welcome to my home page.....</h1>"
+@app.route("/contact")
+def contact():
+    return "welcome to contact page...."
 
+@app.route("/about")
+def about():
+    return "welcome to about page....."
 if(__name__=="__main__"):
-  app.run()
+  app.run(debug=True)
